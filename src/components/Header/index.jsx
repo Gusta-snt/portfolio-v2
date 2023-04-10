a9e51494a2d684d423075d1b71722071c538be62
Gusta-snt <gustavofrsnt@gmail.com>
2023-04-10 19:48:48 -0300

    Create Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import './style.css'
+import Logo from './../../assets/logo.svg'
+
+function Header(){
+    return (
+        <header>
+	    <img src={Logo} alt="Logo"/>
+	    <nav>
+	        <ul>
+		    <li><a href="#">Home</a></li>
+	            <li><a href="#">About</a></li>
+	    	    <li><button>Download CV</button></li>
+	        </ul>
+	    </nav>
+	</header>
+    )
+}
+
+export default Header
