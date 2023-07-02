71950c7700161a17385ed5a39f2c91c9a6f8e303
Gusta-snt <gustavofrsnt@gmail.com>
2023-07-01 21:09:37 -0300

    Add Home responsivity

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,17 +2,22 @@ import profile from './../../assets/profile.jpg'
 import profileBlob from './../../assets/profile-blob.svg'
 
 import './style.css'
+import './responsivity.css'
 
 function Home() {
     return (
         <section className="home">
 		    <div id="texts">
-				<h1>Gustavo<br/>Ferreira</h1>
+			<h1>Gustavo<br/>Ferreira</h1>
 		        <p><span className="subtitle-underline">Fu</span>ll-stack developer</p>
 		    </div>
-		    <div id="image">
-		        <img src={profile} alt="Profile image." id="profile"/>
-		        <img src={profileBlob} id="blob" />
+		    <div id="images">
+	    		<div id="image-profile">
+		        	<img src={profile} alt="Profile image." id="profile"/>
+	    		</div>
+	    		<div id="image-blob">
+		        	<img src={profileBlob} id="blob" />
+	    		</div>
 		    </div>
         </section>
     )
