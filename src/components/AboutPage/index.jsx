2bd1c785c60cd99c044282056a9841cf9dd7f3b7
Gusta <gustavofrsnt@gmail.com>
2023-07-30 11:39:53 -0300

    Add About page

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -1,6 +1,26 @@
+import Header from "./../Header"
+import Footer from "./../Footer"
+import avatar from "./../../assets/gusta-avatar.png"
+
+import "./style.css"
+
 function About() {
 	return (
-		<h1>About</h1>
+		<>
+		<Header/>
+		<main className="about">
+			<aside className="aside-left">
+				<h1><span className="underline">Som</span>ething about me</h1>
+				<p>Hoje, estudante de programação, inicio a carreira como dev Fullstack. Conheci a programação por meio do curso de técnico integrado ao ensino médio de Automação Industrial, onde aprendi lógica de programação e linguagem C. Com o tempo, fui me aprimorando até que conheci HTML, CSS e JS, que são tecnologias importantes para minha carreira. Meu objetivo é me tornar um grande programador no futuro, com a finalidade de fazer com que a vida das pessoas se torne mais fácil, por meio da tecnologia.</p>
+				
+			</aside>
+			<aside className="aside-right">
+				<img src={avatar} alt="Meu avatar."/>
+			</aside>
+		</main>
+		<hr/>
+		<Footer/>
+		</>
 	)
 }
 
