02abdebf10640378aaa099682e69b0254138febe
Gusta <gustavofrsnt@gmail.com>
2023-08-05 11:50:36 -0300

    Add Header responsivity

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,44 @@
+import React, {useState, useEffect} from "react"
+
 import { Link } from "react-router-dom"
 
 import './style.css'
+import './responsivity.css'
 import Logo from './../../assets/logo.svg'
+import MenuIcon from './../../assets/menu-icon.svg'
+import CloseIcon from './../../assets/close-icon.svg'
 
 function Header(){
+	let [classNav, setNav] = useState("off") 
+
+	function check(e) {
+		if(e.target.checked) {
+			setNav("on")
+		}else{
+			setNav("off")
+		}
+	}
+
     return (
-        <header>
+        <header className={classNav}>
         	<div>
-			<img src={Logo} alt="Logo"/>
-			<nav>
-			    <ul className="menu-list">
-				<li><Link to="/" className="header-link" lang="en">Home</Link></li>
-			        <li><Link to="/about" className="header-link">Sobre</Link></li>
-			    	<li><button lang="en">Download CV</button></li>
-			    </ul>
-			</nav>
-		</div>
-	</header>
+				<img src={Logo} alt="Logo"/>
+				<nav className={classNav}>
+				    <ul className="menu-list">
+						<li><Link to="/" className="header-link" lang="en">Home</Link></li>
+				        <li><Link to="/about" className="header-link">Sobre</Link></li>
+				    	<li><button lang="en">Download CV</button></li>
+				    </ul>
+				    <label htmlFor="active" className="close-btn">
+				    	<img src={CloseIcon} alt="Abrir menu"/>
+				    </label>
+				</nav>
+			</div>
+			<input type="checkbox" id="active" onChange={e => check(e)}/>
+			<label htmlFor="active" className="menu-btn">
+				<img src={MenuIcon} alt="Abrir menu"/>
+			</label>
+		</header>
     )
 }
 
