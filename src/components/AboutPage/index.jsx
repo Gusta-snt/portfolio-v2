88b7e9dba1f7cd3648485b3a3a5012a5efce19a8
Gustavo Ferreira <99352903+Gusta-snt@users.noreply.github.com>
2024-08-13 14:43:06 -0300

    Update index.jsx

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -12,7 +12,18 @@ function About() {
 			<main className="about">
 				<aside className="aside-left">
 					<h1><span className="underline">Sob</span>re mim</h1>
-					<p>Hoje, estudante de programação, inicio a carreira como dev Fullstack. Conheci a programação por meio do curso de técnico integrado ao ensino médio de Automação Industrial, onde aprendi lógica de programação e linguagem C. Com o tempo, fui me aprimorando até que conheci HTML, CSS e JS, que são tecnologias importantes para minha carreira. Meu objetivo é me tornar um grande programador no futuro, com a finalidade de fazer com que a vida das pessoas se torne mais fácil, por meio da tecnologia.</p>
+					<p>
+						Atualmente, atuo como Assistente Administrativo na Equatorial Energia Goiás, 
+						onde desenvolvo e implemento projetos de automação e análise de dados para 
+						a área do meio ambiente. Minha experiência inclui o uso de tecnologias como 
+						Excel, Python e Pandas, com interesse crescente em machine learning e banco de dados, 
+						o que me permite aplicar técnicas avançadas de ciência de dados em meus projetos. 
+						Além disso, sou responsável pela organização e gerenciamento de informações relativas a 
+						licenças ambientais e pela emissão de licenças de porte e uso de motosserras em conformidade 
+						com as normas do IBAMA. Complementando minha atuação profissional, também ofereço 
+						aulas particulares em disciplinas como Cálculo e Álgebra Linear, o que contribui para 
+						o aprimoramento constante do meu conhecimento matemático e didático.
+					</p>
 				</aside>
 				<aside className="aside-right">
 					<img src={avatar} alt="Meu avatar."/>
